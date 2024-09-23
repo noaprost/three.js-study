325b2e0708dd5fdf2cfa5835bef30362e36e42f9
noaprost <kangjiwon1850@gmail.com>
2024-09-23 23:50:30 +0900

    three.js physics - 주석 추가:

--- three_js_physics/src/main.js
+++ three_js_physics/src/main.js
@@ -4,6 +4,6 @@
 // import example from "./ex04"; // Perfomance(성능 좋게 하기)
 // import example from "./ex05"; // 충돌 이벤트, 사운드 넣기
 // import example from "./ex06"; // 오브젝트 제거하기
-import example from "./ex07";
+import example from "./ex07"; // 도미노
 
 example();
